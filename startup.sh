ee1abe11d315dfc846d67c26528c133890a039b0
神楽坂白泠 <i@youon.moe>
2020-11-29 22:26:53 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 sudo yum update -y &&
 sudo yum install epel-release -y &&
-sudo yum install screen wget -y &&
+sudo yum install tmux wget -y &&
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2 &&
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&
 sudo yum install -y docker-ce docker-ce-cli containerd.io &&
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 sudo systemctl restart docker && 
 sudo systemctl enable docker &&
